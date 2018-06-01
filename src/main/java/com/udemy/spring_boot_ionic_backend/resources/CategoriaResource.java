4b665afdec52846476b464a807503546d05d19da
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-06-01 14:12:54 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/udemy/spring_boot_ionic_backend/resources/CategoriaResource.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.udemy.spring_boot_ionic_backend.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.udemy.spring_boot_ionic_backend.domain.Categoria;
+import com.udemy.spring_boot_ionic_backend.dto.CategoriaDTO;
 import com.udemy.spring_boot_ionic_backend.services.CategoriaService;
 
 @RestController
@@ -51,5 +54,14 @@ public class CategoriaResource {
 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 	
 }
