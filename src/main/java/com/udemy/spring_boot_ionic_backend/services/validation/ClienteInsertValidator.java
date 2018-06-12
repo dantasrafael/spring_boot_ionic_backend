a97fff49c1b0b8c90bde7122303ad9add01fdf33
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-06-12 13:15:36 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/udemy/spring_boot_ionic_backend/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.udemy.spring_boot_ionic_backend.domain.Cliente;
 import com.udemy.spring_boot_ionic_backend.domain.enums.TipoCliente;
 import com.udemy.spring_boot_ionic_backend.dto.ClienteNewDTO;
+import com.udemy.spring_boot_ionic_backend.repositories.ClienteRepository;
 import com.udemy.spring_boot_ionic_backend.resources.exceptions.FieldMessage;
 import com.udemy.spring_boot_ionic_backend.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {}
@@ -26,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
 		}
 		
 		for (FieldMessage e : list) {
