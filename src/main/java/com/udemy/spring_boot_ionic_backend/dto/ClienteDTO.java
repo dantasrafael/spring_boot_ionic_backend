b7c34fc305f334c744383bf1039360ef7606ed2d
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-06-12 13:55:24 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/udemy/spring_boot_ionic_backend/dto/ClienteDTO.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.udemy.spring_boot_ionic_backend.domain.Cliente;
+import com.udemy.spring_boot_ionic_backend.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
