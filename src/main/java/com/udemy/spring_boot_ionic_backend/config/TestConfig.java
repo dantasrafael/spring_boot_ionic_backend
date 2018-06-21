89e48943df5bc838245e689b33615f08324e398e
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-06-20 22:42:38 -0300

    Criando o profile de teste

---
+++ src/main/java/com/udemy/spring_boot_ionic_backend/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.udemy.spring_boot_ionic_backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.udemy.spring_boot_ionic_backend.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+	
+}
