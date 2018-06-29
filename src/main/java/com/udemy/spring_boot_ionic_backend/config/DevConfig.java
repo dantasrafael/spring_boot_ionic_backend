50c00c4a41b08fe662909cc1411294adbee2ec57
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-06-29 08:27:54 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/udemy/spring_boot_ionic_backend/config/DevConfig.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.udemy.spring_boot_ionic_backend.services.DBService;
+import com.udemy.spring_boot_ionic_backend.services.EmailService;
+import com.udemy.spring_boot_ionic_backend.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,11 @@ public class DevConfig {
 		dbService.instantiateTestDataBase();
 		return true;
 	}
+	
+	@Bean
+	public EmailService emailService( ) {
+		return new SmtpEmailService();
+	}
+	
 	
 }
