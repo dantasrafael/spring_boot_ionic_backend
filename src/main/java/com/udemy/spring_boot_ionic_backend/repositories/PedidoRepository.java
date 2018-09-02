839b8d715235ba5503bd053032e527778e70a74b
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-09-02 15:15:59 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/udemy/spring_boot_ionic_backend/repositories/PedidoRepository.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/repositories/PedidoRepository.java
@@ -1,9 +1,18 @@
 package com.udemy.spring_boot_ionic_backend.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.udemy.spring_boot_ionic_backend.domain.Cliente;
 import com.udemy.spring_boot_ionic_backend.domain.Pedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{}
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+	
+}
--- src/main/java/com/udemy/spring_boot_ionic_backend/services/PedidoService.java
+++ src/main/java/com/udemy/spring_boot_ionic_backend/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.udemy.spring_boot_ionic_backend.domain.Cliente;
 import com.udemy.spring_boot_ionic_backend.domain.ItemPedido;
 import com.udemy.spring_boot_ionic_backend.domain.PagamentoComBoleto;
 import com.udemy.spring_boot_ionic_backend.domain.Pedido;
@@ -14,6 +18,8 @@ import com.udemy.spring_boot_ionic_backend.domain.enums.EstadoPagamento;
 import com.udemy.spring_boot_ionic_backend.repositories.ItemPedidoRepository;
 import com.udemy.spring_boot_ionic_backend.repositories.PagamentoRepository;
 import com.udemy.spring_boot_ionic_backend.repositories.PedidoRepository;
+import com.udemy.spring_boot_ionic_backend.security.UserSS;
+import com.udemy.spring_boot_ionic_backend.services.exceptions.AuthorizationException;
 import com.udemy.spring_boot_ionic_backend.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,6 +76,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 
 }
