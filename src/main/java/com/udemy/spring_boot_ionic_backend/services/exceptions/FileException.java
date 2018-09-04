821187522c5f9a742f56d13eeee7dc79a3615f3d
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-09-03 22:05:20 -0300

    Tratando excecoes adequadamente

---
+++ src/main/java/com/udemy/spring_boot_ionic_backend/services/exceptions/FileException.java
@@ -0,0 +1,15 @@
+package com.udemy.spring_boot_ionic_backend.services.exceptions;
+
+public class FileException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
